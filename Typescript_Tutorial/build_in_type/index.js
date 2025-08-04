ce19b05e929a5c2a50e8442177c651cabd27fa1d
AklimaHakkani123 <aklimahakkani987@gmail.com>
2025-08-04 02:59:52 -0700

    Your message

---
+++ Typescript_Tutorial/build_in_type/index.js
@@ -0,0 +1,14 @@
+//build in-->type (number,string,bolean,void,null,undefind)
+var userId;
+var userName;
+var isActivated;
+userId = 123;
+userName = 'hima hakkani';
+isActivated = true;
+console.log(userId);
+console.log(userName);
+console.log(isActivated);
+function display() {
+    console.log('hello i am hima hakkani eva');
+}
+display();
---
+++ Typescript_Tutorial/build_in_type/index.ts
@@ -0,0 +1,17 @@
+//build in-->type (number,string,bolean,void,null,undefind)
+let userId:number;
+let userName:string;
+let isActivated:boolean;
+
+userId=123;
+userName='hima hakkani';
+isActivated=true;
+console.log(userId);
+console.log(userName);
+console.log(isActivated);
+function display():void{
+    console.log('hello i am hima hakkani eva');
+    
+}
+display();
+
